1d0999ea11ef5721a4a524b620e432c4e4eaddd0
bridgemami <mdbridgeman@gmail.com>
2023-05-23 14:16:26 -0700

    changed resume file name

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Nav from "../../../../node_modules/react-bootstrap/Nav";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
-import resume from "../../../../src/assets/Resume_5_23.pdf";
+import resume from "../../../../src/assets/Michael-Bridgeman-Resume.pdf";
 import s from "./links.module.css";
 
 export default function Links() {
