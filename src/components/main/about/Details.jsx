6653b37af995577ead0cbfb2d17d3218f6041274
bridgemami <mdbridgeman@gmail.com>
2023-06-01 17:23:15 -0700

    updated the details component

--- src/components/main/about/Details.jsx
+++ src/components/main/about/Details.jsx
@@ -3,43 +3,16 @@ export default function Details() {
     <article>
       <h2>About</h2>
       <p>
-        Ready to embark on an exciting journey in the world of web development?
-        Look no further! I am Michael Bridgeman, a web developer born and raised
-        in the vibrant Bay Area. With a bachelor&apos;s degree in business from
-        Sonoma State University, I have spent over a decade honing my skills in
-        the retail, hospitality, and customer service industries, always driven
-        by my passion for delivering exceptional experiences to people. It was
-        during my involvement in launching the tablet program at Black Stallion
-        Estate Winery that I discovered my fascination with the transformative
-        power of technology in streamlining business operations and enhancing
-        efficiency.
+      Ready to embark on an exciting journey in the world of web development? Look no further! With a bachelor&apos;s degree in business from Sonoma State University and over a decade of experience in the retail, hospitality, and customer service industries, I have honed my skills in delivering exceptional experiences to people. During my involvement in launching the tablet program at Black Stallion Estate Winery, I discovered my fascination with the transformative power of technology in streamlining business operations and enhancing efficiency.
       </p>
       <p>
-        During the pandemic, a former manager suggested I take an HTML and CSS
-        class to broaden my technology skills. That is when my journey into web
-        development began, and I was instantly captivated. Exploring the
-        intricacies of website structure, design, and interactivity became my
-        new passion. Motivated to deepen my knowledge, I pursued an Associate in
-        Science degree in Web Full-Stack Development from Santa Rosa Junior
-        College, graduating with honors in December 2022.
+      During the pandemic, a former manager suggested I take an HTML and CSS class to broaden my technology skills. That is when my journey into web development began, and I was instantly captivated. Exploring the intricacies of website structure, design, and interactivity became my new passion. Motivated to deepen my knowledge, I pursued an Associate in Science degree in Web Full-Stack Development from Santa Rosa Junior College, graduating with honors in December 2022.
       </p>
       <p>
-        Beyond the realm of web development, I am an avid explorer of the world
-        around me. I have traversed over 25 countries in Europe and South
-        America, immersing myself in diverse cultures. Closer to home, I have
-        ventured through more than 25 states, appreciating the rich tapestry of
-        the United States. When I am not immersed in coding, you will likely
-        find me hiking amidst the breathtaking beauty of the North Bay, seeking
-        inner harmony through yoga, lacing up my running shoes for a
-        revitalizing jog, or cherishing precious moments with my loved ones. In
-        addition, I have a deep interest in learning about cultures, astronomy,
-        and history, with a sprinkle of Star Wars and Lord of the Rings fandom.
+      Beyond the realm of web development, I am an avid explorer of the world around me. Having traveled through over 25 countries in Europe and South America, I immerse myself in diverse cultures. Closer to home, I have ventured through more than 25 states, appreciating the rich tapestry of the United States. When I am not immersed in coding, you will likely find me hiking amidst the breathtaking beauty of the North Bay, seeking inner harmony through yoga, lacing up my running shoes for a revitalizing jog, or cherishing precious moments with my loved ones. In addition, I have a deep interest in learning about cultures, astronomy, and history, with a sprinkle of Star Wars and Lord of the Rings fandom.
       </p>
       <p>
-        I am truly excited to share my web development projects and experiences
-        with you through this portfolio. Let us embark on a captivating journey
-        together, exploring the limitless possibilities at the intersection of
-        creativity, technology, and human connection.
+      I am truly excited to share my web development projects and experiences with you through this portfolio. Let us embark on a captivating journey together, exploring the limitless possibilities at the intersection of creativity, technology, and human connection.
       </p>
     </article>
   );
